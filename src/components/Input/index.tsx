00c27548467d2da90a5c8c292dae916ca327d34d
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-03-22 14:55:45 -0300

    Component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+import { InputHTMLAttributes } from "react"
+import { InputComponent } from "./styles"
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+export const Input = ({ ...rest }: InputProps) => {
+    return (
+        <InputComponent {...rest} />
+    )
+}
