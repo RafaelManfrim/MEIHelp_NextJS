c08aa109a29845e428ea13bacfce47d963cec3e9
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-06-20 16:01:41 -0300

    Criando página de estoque

---
+++ src/pages/stock/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Base } from '../../components/template'
+
+const Stock: NextPage = () => {
+    return (
+        <Base>
+            Hello world
+        </Base>
+    )
+}
+
+export default Stock
