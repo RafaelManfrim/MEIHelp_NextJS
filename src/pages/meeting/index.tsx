85a850ad25fc77e610f2643936d9dc200aa6c91c
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-06-20 16:01:59 -0300

    criando página de encontrar outros MEIs

---
+++ src/pages/meeting/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Base } from '../../components/template'
+
+const Meeting: NextPage = () => {
+    return (
+        <Base>
+            Hello world
+        </Base>
+    )
+}
+
+export default Meeting
