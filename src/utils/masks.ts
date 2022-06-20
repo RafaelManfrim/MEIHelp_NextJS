30ef3c217ca30c8ab9a97aab7d5c12705599dca1
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-06-20 15:56:11 -0300

    Iniciando máscara de telefone

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -7,3 +7,8 @@ export const cnpjMask = (value: string) => {
       .replace(/(\d{4})(\d)/, '$1-$2')
       .replace(/(-\d{2})\d+?$/, '$1') // captura os dois últimos 2 números, com um - antes dos dois números
 }
+
+export const phoneMask = (value: string) => {
+  return value
+  .replace(/\D+/g, '') // não deixa ser digitado nenhuma letra
+}
