4920487e21d40d656309946a58a65e78ce2e2fc7
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-06-29 17:22:44 -0300

    ajustes tokens e interceptor

--- src/pages/activities/index.tsx
+++ src/pages/activities/index.tsx
@@ -1,49 +1,34 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 import { IoCheckmarkSharp, IoPencilSharp, IoTrashSharp } from 'react-icons/io5'
 import { Button } from '../../components/Button'
 import { Base } from '../../components/template'
+import { api } from '../../services/api'
 
 import { ActivitiesContainer, ActivityButton, ActivityButtons, ActivityContainer, ActivityCreatedAt, ActivityDescription, ActivityDetails, ActivityInfos, ActivityStatus, ActivityStatusColor, ActivityTitle } from './styles'
 
+interface Activity {
+    id: number
+    title: string
+    description: string
+    finished: boolean
+    forecast_date: string
+    created_at: string
+    updated_at: string
+    finished_at: string
+}
+
 const Activities: NextPage = () => {
-    const activities = [
-        {
-            id: 1,
-            title: 'Teste',
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            finished: false,
-            forecast_date: new Date(),
-            created_at: new Date(),
-            finished_at: new Date(),
-        },
-        {
-            id: 2,
-            title: 'Teste 2',
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            finished: false,
-            forecast_date: new Date(),
-            created_at: new Date(),
-            finished_at: new Date(),
-        },
-        {
-            id: 3,
-            title: 'Teste 3',
-            description: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            finished: true,
-            forecast_date: new Date(),
-            created_at: new Date(),
-            finished_at: new Date(),
-        },
-        {
-            id: 4,
-            title: 'Teste 4',
-            description: "",
-            finished: true,
-            forecast_date: new Date(),
-            created_at: new Date(),
-            finished_at: new Date(),
-        },
-    ]
+    const [activities, setActivities] = useState<Activity[]>([])
+
+    useEffect(() => {
+        async function loadActivities() {
+            const response = await api.get('/activities/')
+            setActivities(response.data)
+        }
+
+        loadActivities()
+    }, [])
 
     return (
         <Base>
@@ -58,8 +43,8 @@ const Activities: NextPage = () => {
                                 <ActivityDescription>{activity.description ? activity.description : 'Não há descrição.'}</ActivityDescription>
                             </ActivityDetails>
                             <ActivityInfos>
-                                <ActivityCreatedAt>Criado em: {activity.created_at.toLocaleDateString()}</ActivityCreatedAt>
-                                <ActivityStatus>{activity.finished ? `Finalizado: ${activity.finished_at.toLocaleDateString()}` : `Previsão de conclusão: ${activity.forecast_date.toLocaleDateString()}`}</ActivityStatus>
+                                <ActivityCreatedAt>Criado em: {activity.created_at}</ActivityCreatedAt>
+                                <ActivityStatus>{activity.finished ? `Finalizado: ${activity.finished_at}` : `Previsão de conclusão: ${activity.forecast_date}`}</ActivityStatus>
                                 <ActivityButtons>
                                     {!activity.finished && (
                                         <>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,74 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+import Router from 'next/router'
+import { destroyCookie, parseCookies, setCookie } from 'nookies'
+
+let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue: any[] = []
 
 export const api = axios.create({
-    baseURL: 'http://0.0.0.0:8001/api'
+    baseURL: 'http://0.0.0.0:8001/api',
+    headers: {
+        Authorization: `Bearer ${cookies['meihelp.accessToken']}`
+    }
+})
+
+api.interceptors.response.use(response => {
+    return response
+}, (error: AxiosError) => {
+    if (error.response?.status === 401) {
+        if (error.response?.data?.code === 'token_not_valid') {
+            cookies = parseCookies()
+            const { 'meihelp.refreshToken': refreshToken } = cookies
+            const originalConfig = error.config
+
+            if (!isRefreshing) {
+                isRefreshing = true
+
+                api.post('/token/refresh/', { refresh: refreshToken }).then(response => {
+                    const newToken = response.data.access
+
+                    setCookie(undefined, 'meihelp.accessToken', newToken, {
+                        maxAge: 1000 * 60 * 5,
+                        path: '/'
+                    })
+
+                    api.defaults.headers.common['Authorization'] = `Bearer ${newToken}`
+
+                    failedRequestsQueue.forEach(req => req.resolve(newToken))
+                    failedRequestsQueue = []
+
+                }).catch(err => {
+                    failedRequestsQueue.forEach(req => req.reject(err))
+                    failedRequestsQueue = []
+
+                }).finally(() => {
+                    isRefreshing = false
+                })
+            }
+
+            return new Promise((resolve, reject) => {
+                failedRequestsQueue.push({
+                    resolve: (token: string) => {
+                        if (originalConfig.headers) {
+                            originalConfig.headers['Authorization'] = `Bearer ${token}`
+                            resolve(api(originalConfig))
+                        }
+                    },
+                    reject: (err: AxiosError) => {
+                        reject(err)
+                    }
+                })
+            })
+
+        } else {
+            destroyCookie(undefined, 'meihelp.accessToken')
+            destroyCookie(undefined, 'meihelp.refreshToken')
+            if (!Router.asPath.startsWith('/login')) {
+                Router.push('/login')
+            }
+        }
+    }
+
+    return Promise.reject(error)
 })
