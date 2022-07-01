3be03429119c209b16f57a6986e60c2420c2bd27
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-01 11:55:39 -0300

    exibindo dados no meeting

--- src/pages/meeting/index.tsx
+++ src/pages/meeting/index.tsx
@@ -1,10 +1,61 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 import { Base } from '../../components/template'
+import { api } from '../../services/api'
+import { cnpjMask } from '../../utils/masks'
+import { UserContactInfo, UserContainer, UserInfo, UserLocalizationInfo, UsersContainer } from './styles'
+
+interface User {
+    id: number
+    corporate_name: string
+    description: string
+    cnpj: string
+    city: string
+    cep: string
+    uf: string
+    email: string
+    phone: string
+}
 
 const Meeting: NextPage = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const response = await api.get('/companies/all/')
+                setUsers(response.data)
+            } catch (err) {
+                toast.error('Houve um erro ao encontrar outros MEIs')
+                console.log(err)
+            }
+        }
+        
+        fetchUsers()
+    }, [])
+
     return (
         <Base>
-            Hello world
+            <UsersContainer>
+                {users.map(user => (
+                    <UserContainer key={user.id}>
+                        <UserInfo>
+                            <strong>{user.corporate_name}</strong>
+                            <p>CNPJ: {cnpjMask(user.cnpj)}</p>
+                            <span>{user.description}</span>
+                        </UserInfo>
+                        <UserLocalizationInfo>
+                            <strong>{user.city} - {user.uf}</strong>
+                            <p>{user.cep}</p>
+                        </UserLocalizationInfo>
+                        <UserContactInfo>
+                            <strong>{user.email}</strong>
+                            <strong>{user.phone}</strong>
+                        </UserContactInfo>
+                    </UserContainer>
+                ))}
+            </UsersContainer>
         </Base>
     )
 }
--- src/pages/meeting/styles.ts
+++ src/pages/meeting/styles.ts
@@ -1,1 +1,24 @@
 import styled from 'styled-components'
+
+export const UsersContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
+
+export const UserContainer = styled.div`
+    background: var(--white);
+    width: 100%;
+    padding: 1rem;
+    border-radius: 8px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
+`
+
+export const UserInfo = styled.div``
+
+export const UserLocalizationInfo = styled.div``
+
+export const UserContactInfo = styled.div``
