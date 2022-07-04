d0699808860e3f1d66dd684204933aafc26262b8
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-04 10:05:44 -0300

    adicionando estilos react modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -56,4 +56,37 @@ export const GlobalStyle = createGlobalStyle`
         opacity: 0.6;
         cursor: not-allowed;
     }
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--white);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.5rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+        transition: filter 0.2s;
+        &:hover {
+            filter: brightness(0.8);
+        }
+    }
 `
