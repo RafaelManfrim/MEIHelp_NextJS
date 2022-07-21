3b9a58bb8fccab9764a3468e1974f807103f4aee
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-21 10:18:00 -0300

    algumas mudanças página profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,35 +1,92 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from '../../components/Button'
 import { Base } from '../../components/template'
 import { useAuth } from '../../contexts/AuthContext'
+import { cepMask, phoneMask } from '../../utils/masks'
+import { normalizeCep, normalizePhone } from '../../utils/normalizers'
 
-import { CepContainer, DescriptionContainer, EmailContainer, MainContainer, NameContainer, PhoneContainer, ProfileContainer, ProfileTitle } from './styles'
+import {
+  CepInput,
+  DescriptionContainer,
+  EmailInput,
+  InputControl,
+  InputLabel,
+  MainContainer,
+  NameContainer,
+  PhoneInput,
+  ProfileContainer,
+  ProfileTitle,
+  Row,
+} from './styles'
 
 const Profile: NextPage = () => {
-    const { user } = useAuth()
-    const [email, setEmail] = useState(user.email)
-    const [phone, setPhone] = useState(user.phone)
-    const [cep, srtCep] = useState(user.cep)
+  const { user } = useAuth()
+  const [email, setEmail] = useState(user.email || '')
+  const [phone, setPhone] = useState(user.phone || '')
+  const [cep, setCep] = useState(user.cep || '')
+  const [description, setDescription] = useState(user.description || '')
 
-    return (
-        <Base>
-            <MainContainer>
-                <ProfileTitle>
-                    Perfil
-                </ProfileTitle>
-                <ProfileContainer>
-                    Nome da empresa: <NameContainer>{user.corporate_name}</NameContainer>
-                    Descrição: <DescriptionContainer defaultValue={user.description || ''} />
-                    Email: <EmailContainer value={email} />
-                    Celular: <PhoneContainer value={phone} />
-                    CEP: <CepContainer value={cep} />
-                    <Button text="Salvar alterações" />
-                    <Button color='red' text="Excluir sua conta" style={{ marginTop: 16 }} />
-                </ProfileContainer>
-            </MainContainer>
-        </Base>
-    )
+  async function handleSaveChanges() {
+    console.log(email, normalizePhone(phone), normalizeCep(cep), description)
+  }
+
+  async function handleDeleteAccount() {
+    console.log('delete account')
+  }
+
+  useEffect(() => {
+    setEmail(user.email || '')
+    setPhone(user.phone || '')
+    setCep(user.cep || '')
+    setDescription(user.description || '')
+  }, [user])
+
+  return (
+    <Base>
+      <MainContainer>
+        <ProfileTitle>Perfil</ProfileTitle>
+        <ProfileContainer>
+          Nome da empresa: <NameContainer>{user.corporate_name}</NameContainer>
+          Descrição:{' '}
+          <DescriptionContainer
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <Row>
+            <InputControl>
+              <InputLabel>Email:</InputLabel>
+              <EmailInput
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </InputControl>
+            <InputControl>
+              <InputLabel>Celular:</InputLabel>
+              <PhoneInput
+                value={phoneMask(phone)}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </InputControl>
+            <InputControl>
+              <InputLabel>CEP:</InputLabel>
+              <CepInput
+                value={cepMask(cep)}
+                onChange={(e) => setCep(e.target.value)}
+              />
+            </InputControl>
+          </Row>
+          <Button text="Salvar alterações" onClick={handleSaveChanges} />
+          <Button
+            color="red"
+            text="Excluir sua conta"
+            style={{ marginTop: 16 }}
+            onClick={handleDeleteAccount}
+          />
+        </ProfileContainer>
+      </MainContainer>
+    </Base>
+  )
 }
 
 export default Profile
--- src/pages/profile/styles.ts
+++ src/pages/profile/styles.ts
@@ -1,62 +1,69 @@
 import styled from 'styled-components'
 
 export const MainContainer = styled.div`
-    width: 100%;
-    max-width: 1000px;
+  width: 100%;
+  max-width: 1000px;
 `
 
 export const ProfileTitle = styled.div`
-    font-size: 1.5rem;
-    font-weight: bold;
+  font-size: 1.5rem;
+  font-weight: bold;
 `
 
 export const ProfileContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 1.5rem;
-    font-size: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.5rem;
+  font-size: 1rem;
 `
 
 export const NameContainer = styled.span`
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-    font-size: 1rem;
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+  font-size: 1rem;
 `
 
 export const DescriptionContainer = styled.textarea`
-    resize: none;
-    background: transparent;
-    height: 10vh;
-    outline: none;
-    border: 1px solid var(--black);
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-    padding: 0.5rem;
+  resize: none;
+  background: transparent;
+  height: 10vh;
+  outline: none;
+  border: 1px solid var(--dark);
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
 `
 
-export const EmailContainer = styled.input`
-    background: transparent;
-    outline: none;
-    border: 1px solid var(--black);
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-    padding: 0.5rem;
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto auto;
+  gap: 0.5rem;
 `
 
-export const PhoneContainer = styled.input`
-    background: transparent;
-    outline: none;
-    border: 1px solid var(--black);
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-    padding: 0.5rem;
+export const InputControl = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 `
 
-export const CepContainer = styled.input`
-    background: transparent;
-    outline: none;
-    border: 1px solid var(--black);
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-    padding: 0.5rem;
+export const InputLabel = styled.label``
+
+const BaseInput = styled.input`
+  background: transparent;
+  outline: none;
+  border: 1px solid var(--dark);
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  flex: 1;
+`
+
+export const EmailInput = styled(BaseInput)``
+
+export const PhoneInput = styled(BaseInput)`
+  max-width: 12rem;
+`
+
+export const CepInput = styled(BaseInput)`
+  max-width: 12rem;
 `
