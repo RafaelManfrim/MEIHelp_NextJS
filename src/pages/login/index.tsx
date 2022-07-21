c823d4a94a9a96ede6d0c598158acd0ec798d899
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-21 10:17:18 -0300

    alguns ajustes página de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,197 +5,251 @@ import toast from 'react-hot-toast'
 
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
-import bgImage from "../../../public/bg-login.jpg"
-import logo from "../../../public/logo.png"
+import bgImage from '../../../public/bg-login.jpg'
+import logo from '../../../public/logo.png'
 import { apiNoAuth } from '../../services/api'
 import { cnpjMask, phoneMask } from '../../utils/masks'
 import { useAuth } from '../../contexts/AuthContext'
+import { normalizeCnpj } from '../../utils/normalizers'
 
-import { LoginPageContainer, LoginFormContainer, LoginPageImageContainer, LoginForm, ChangeModeText, ChangeModeLink, LogoLogin, FormDescription, ErrorInformation } from './styles'
+import {
+  LoginPageContainer,
+  LoginFormContainer,
+  LoginPageImageContainer,
+  LoginForm,
+  ChangeModeText,
+  ChangeModeLink,
+  LogoLogin,
+  FormDescription,
+  ErrorInformation,
+} from './styles'
 
 const Login: NextPage = () => {
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [error, setError] = useState('')
-    const [registerStep, setRegisterStep] = useState<1 | 2>(1)
-    const [loading, setLoading] = useState(false)
-    const [cnpj, setCnpj] = useState('')
-    const [normalizedCnpj, setNormalizedCnpj] = useState('')
-    const [email, setEmail] = useState('')
-    const [phoneNumber, setPhoneNumber] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [isRegistering, setIsRegistering] = useState(false)
+  const [error, setError] = useState('')
+  const [registerStep, setRegisterStep] = useState<1 | 2>(1)
+  const [loading, setLoading] = useState(false)
+  const [cnpj, setCnpj] = useState('')
+  const [normalizedCnpj, setNormalizedCnpj] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
 
-    const { signIn } = useAuth()
+  const { signIn } = useAuth()
 
-    async function handleNextStep() {
-        try {
-            setLoading(true)
-            const normalizedCnpj = cnpj.replaceAll('.', '').replace('/', '').replace('-', '')
-            await apiNoAuth.post('/cnpj/validate/', { cnpj: normalizedCnpj })
-            setNormalizedCnpj(normalizedCnpj)
-            setEmail('')
-            setPhoneNumber('')
-            setCnpj('')
-            setError('')
-            setRegisterStep(2)
-        } catch (err: any) {
-            if (err.response.status === 400) {
-                setError('O CNPJ informado é inválido ou ainda não foi cadastrado no Sefaz.')
-            } else if (err.response.status === 401) {
-                setError('O CNPJ informado precisa ser referente a uma MEI.')
-            } else if (err.response.status === 429) {
-                setError('O sistema está sobrecarregado, por favor, aguarde um instante.')
-            } else if (err.response.status === 504) {
-                setError('O sistema está indisponível, por favor, tente novamente mais tarde.')
-            } else {
-                setError('Houve um erro inesperado, pedimos desculpas, por favor tente novamente mais tarde.')
-            }
-        } finally {
-            setLoading(false)
-        }
+  async function handleNextStep() {
+    try {
+      setLoading(true)
+      const normalizedCnpj = normalizeCnpj(cnpj)
+      await apiNoAuth.post('/cnpj/validate/', { cnpj: normalizedCnpj })
+      setNormalizedCnpj(normalizedCnpj)
+      setEmail('')
+      setPhoneNumber('')
+      setCnpj('')
+      setError('')
+      setRegisterStep(2)
+    } catch (err: any) {
+      if (err.response.status === 400) {
+        setError(
+          'O CNPJ informado é inválido ou ainda não foi cadastrado no Sefaz.',
+        )
+      } else if (err.response.status === 401) {
+        setError('O CNPJ informado precisa ser referente a uma MEI.')
+      } else if (err.response.status === 429) {
+        setError(
+          'O sistema está sobrecarregado, por favor, aguarde um instante.',
+        )
+      } else if (err.response.status === 504) {
+        setError(
+          'O sistema está indisponível, por favor, tente novamente mais tarde.',
+        )
+      } else {
+        setError(
+          'Houve um erro inesperado, pedimos desculpas, por favor tente novamente mais tarde.',
+        )
+      }
+    } finally {
+      setLoading(false)
     }
+  }
 
-    async function handleRegisterAccount() {
-        try {
-            if (password !== passwordConfirm) {
-                setError('As senhas precisam ser iguais.')
-                return
-            }
+  async function handleRegisterAccount() {
+    try {
+      if (password !== passwordConfirm) {
+        setError('As senhas precisam ser iguais.')
+        return
+      }
 
-            const data = {
-                cnpj: normalizedCnpj,
-                phone: phoneNumber,
-                email,
-                password,
-            }
+      const data = {
+        cnpj: normalizedCnpj,
+        phone: phoneNumber,
+        email,
+        password,
+      }
 
-            const response = await apiNoAuth.post('/companies/register/', { ...data })
+      const response = await apiNoAuth.post('/companies/register/', { ...data })
 
-            if (response.status === 201) {
-                toast.success('Sua conta foi criada!')
-                setCnpj('')
-                setNormalizedCnpj('')
-                setEmail('')
-                setPhoneNumber('')
-                setPassword('')
-                setPasswordConfirm('')
-                setError('')
-                setIsRegistering(false)
-            }
-        } catch (err: any) {
-            if (err.response.status === 400) {
-                setError('O CNPJ informado é inválido ou ainda não foi cadastrado no Sefaz.')
-            } else if (err.response.status === 401) {
-                setError('O CNPJ informado precisa ser referente a uma MEI.')
-            } else if (err.response.status === 429) {
-                setError('O sistema está sobrecarregado, por favor, aguarde um instante.')
-            } else if (err.response.status === 504) {
-                setError('O sistema está indisponível, por favor, tente novamente mais tarde.')
-            } else {
-                setError('Houve um erro inesperado, pedimos desculpas, por favor tente novamente mais tarde.')
-            }
-        }
-    }
-
-    function handleChangeIsRegistering() {
+      if (response.status === 201) {
+        toast.success('Sua conta foi criada!')
         setCnpj('')
         setNormalizedCnpj('')
+        setEmail('')
+        setPhoneNumber('')
         setPassword('')
         setPasswordConfirm('')
         setError('')
-        setIsRegistering(!isRegistering)
-        if (registerStep === 2) {
-            setRegisterStep(1)
-        }
+        setIsRegistering(false)
+      }
+    } catch (err: any) {
+      if (err.response.status === 400) {
+        setError(
+          'O CNPJ informado é inválido ou ainda não foi cadastrado no Sefaz.',
+        )
+      } else if (err.response.status === 401) {
+        setError('O CNPJ informado precisa ser referente a uma MEI.')
+      } else if (err.response.status === 429) {
+        setError(
+          'O sistema está sobrecarregado, por favor, aguarde um instante.',
+        )
+      } else if (err.response.status === 504) {
+        setError(
+          'O sistema está indisponível, por favor, tente novamente mais tarde.',
+        )
+      } else {
+        setError(
+          'Houve um erro inesperado, pedimos desculpas, por favor tente novamente mais tarde.',
+        )
+      }
     }
+  }
 
-    async function handleSignIn() {
-        setLoading(true)
-        const normalizedCnpj = cnpj.replaceAll('.', '').replace('/', '').replace('-', '')
-        try {
-            await signIn(normalizedCnpj, password)
-            setLoading(false)
-        } catch (err: any) {
-            if (err.message === 'No active account found with the given credentials') {
-                setError('O e-mail ou a senha são inválidos.')
-            } else {
-                setError('Houve um erro inesperado')
-            }
-        }
+  function handleChangeIsRegistering() {
+    setCnpj('')
+    setNormalizedCnpj('')
+    setPassword('')
+    setPasswordConfirm('')
+    setError('')
+    setIsRegistering(!isRegistering)
+    if (registerStep === 2) {
+      setRegisterStep(1)
     }
+  }
 
-    return (
-        <LoginPageContainer>
-            <LoginFormContainer>
-                {isRegistering ? (
-                    <LoginForm>
-                        <LogoLogin>
-                            <Image src={logo} alt="MEIHelp" layout="responsive" />
-                        </LogoLogin>
-                        {registerStep === 1 ? (
-                            <>
-                                <FormDescription>
-                                    Para começar, precisamos do seu CNPJ.
-                                </FormDescription>
-                                <Input
-                                    type="text"
-                                    value={cnpjMask(cnpj)}
-                                    onChange={e => setCnpj(e.target.value)}
-                                    placeholder="CNPJ"
-                                />
-                                {error && <ErrorInformation>{error}</ErrorInformation>}
-                                <Button text='Continuar' onClick={handleNextStep} />
-                            </>
-                        ) : (
-                            <>
-                                <FormDescription>
-                                    Agora precisamos de mais informações.
-                                </FormDescription>
-                                <Input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} />
-                                <Input placeholder="Celular" value={phoneMask(phoneNumber)} onChange={e => setPhoneNumber(e.target.value)} />
-                                <Input type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
-                                <Input type="password" placeholder="Confirme sua senha" value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value)} />
-                                {error && <ErrorInformation>{error}</ErrorInformation>}
-                                <Button text='Registrar-se' onClick={handleRegisterAccount} />
-                            </>
-                        )}
-                        <ChangeModeText>
-                            Já possui uma conta? <ChangeModeLink onClick={handleChangeIsRegistering}>Clique aqui!</ChangeModeLink>
-                        </ChangeModeText>
-                    </LoginForm>
-                ) : (
-                    <LoginForm>
-                        <LogoLogin>
-                            <Image src={logo} alt="MEIHelp" layout="responsive" />
-                        </LogoLogin>
-                        <FormDescription>
-                            Digite suas credenciais para acessar o sistema.
-                        </FormDescription>
-                        <Input
-                            type="text"
-                            value={cnpjMask(cnpj)}
-                            onChange={e => setCnpj(e.target.value)}
-                            placeholder="CNPJ"
-                        />
-                        <Input
-                            type="password"
-                            placeholder="Senha"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        {error && <ErrorInformation>{error}</ErrorInformation>}
-                        <Button text="Entrar" onClick={handleSignIn} />
-                        <ChangeModeText>
-                            Ainda não possui uma conta? <ChangeModeLink onClick={handleChangeIsRegistering}>Registre-se!</ChangeModeLink>
-                        </ChangeModeText>
-                    </LoginForm>
-                )}
-            </LoginFormContainer>
-            <LoginPageImageContainer>
-                <Image src={bgImage} alt="" layout="fill" />
-            </LoginPageImageContainer>
-        </LoginPageContainer>
-    )
+  async function handleSignIn() {
+    setLoading(true)
+    const normalizedCnpj = normalizeCnpj(cnpj)
+    try {
+      await signIn(normalizedCnpj, password)
+      setLoading(false)
+    } catch (err: any) {
+      if (
+        err.message === 'No active account found with the given credentials'
+      ) {
+        setError('O e-mail ou a senha são inválidos.')
+      } else {
+        setError('Houve um erro inesperado')
+      }
+    }
+  }
+
+  return (
+    <LoginPageContainer>
+      <LoginFormContainer>
+        {isRegistering ? (
+          <LoginForm>
+            <LogoLogin>
+              <Image src={logo} alt="MEIHelp" layout="responsive" />
+            </LogoLogin>
+            {registerStep === 1 ? (
+              <>
+                <FormDescription>
+                  Para começar, precisamos do seu CNPJ.
+                </FormDescription>
+                <Input
+                  type="text"
+                  value={cnpjMask(cnpj)}
+                  onChange={(e) => setCnpj(e.target.value)}
+                  placeholder="CNPJ"
+                />
+                {error && <ErrorInformation>{error}</ErrorInformation>}
+                <Button text="Continuar" onClick={handleNextStep} />
+              </>
+            ) : (
+              <>
+                <FormDescription>
+                  Agora precisamos de mais informações.
+                </FormDescription>
+                <Input
+                  type="email"
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Celular"
+                  value={phoneMask(phoneNumber)}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+                <Input
+                  type="password"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                  type="password"
+                  placeholder="Confirme sua senha"
+                  value={passwordConfirm}
+                  onChange={(e) => setPasswordConfirm(e.target.value)}
+                />
+                {error && <ErrorInformation>{error}</ErrorInformation>}
+                <Button text="Registrar-se" onClick={handleRegisterAccount} />
+              </>
+            )}
+            <ChangeModeText>
+              Já possui uma conta?{' '}
+              <ChangeModeLink onClick={handleChangeIsRegistering}>
+                Clique aqui!
+              </ChangeModeLink>
+            </ChangeModeText>
+          </LoginForm>
+        ) : (
+          <LoginForm>
+            <LogoLogin>
+              <Image src={logo} alt="MEIHelp" layout="responsive" />
+            </LogoLogin>
+            <FormDescription>
+              Digite suas credenciais para acessar o sistema.
+            </FormDescription>
+            <Input
+              type="text"
+              value={cnpjMask(cnpj)}
+              onChange={(e) => setCnpj(e.target.value)}
+              placeholder="CNPJ"
+            />
+            <Input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <ErrorInformation>{error}</ErrorInformation>}
+            <Button text="Entrar" onClick={handleSignIn} />
+            <ChangeModeText>
+              Ainda não possui uma conta?{' '}
+              <ChangeModeLink onClick={handleChangeIsRegistering}>
+                Registre-se!
+              </ChangeModeLink>
+            </ChangeModeText>
+          </LoginForm>
+        )}
+      </LoginFormContainer>
+      <LoginPageImageContainer>
+        <Image src={bgImage} alt="" layout="fill" />
+      </LoginPageImageContainer>
+    </LoginPageContainer>
+  )
 }
 
 export default Login
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,61 +1,61 @@
 import styled from 'styled-components'
 
 export const LoginPageContainer = styled.div`
-    height: 100vh;
-    width: 100%;
-    display: flex;
+  height: 100vh;
+  width: 100%;
+  display: flex;
 `
 
 export const LoginFormContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 35%;
-    padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 35%;
+  padding: 2rem;
 `
 
 export const LoginForm = styled.form`
-    width: 70%;
-    height: 100%;
-    display: flex;
-    flex-direction: column; 
-    align-items: center;
-    justify-content: center;
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 
 export const LogoLogin = styled.div`
-    width: 70%;
-    display: block;
+  width: 70%;
+  display: block;
 `
 
 export const FormDescription = styled.span`
-    font-size: 0.9rem;
-    margin-bottom: 1rem;
-    color: #555;
+  font-size: 0.9rem;
+  margin-bottom: 1rem;
+  color: #555;
 `
 
 export const ChangeModeText = styled.span`
-    margin-top: 1rem;
+  margin-top: 1rem;
 `
 
 export const ChangeModeLink = styled.a`
-    color: var(--blue);
-    cursor: pointer;
-    transition: filter 0.2s;
+  color: var(--blue);
+  cursor: pointer;
+  transition: filter 0.2s;
 
-    &:hover {
-        filter: brightness(0.9);
-    }
+  &:hover {
+    filter: brightness(0.9);
+  }
 `
 
 export const LoginPageImageContainer = styled.div`
-    width: 65%;
-    height: 100%;
-    position: relative;
+  width: 65%;
+  height: 100%;
+  position: relative;
 `
 
 export const ErrorInformation = styled.span`
-    color: var(--red);
-    text-align: center;
-    margin-bottom: 1rem;
+  color: var(--red);
+  text-align: center;
+  margin-bottom: 1rem;
 `
