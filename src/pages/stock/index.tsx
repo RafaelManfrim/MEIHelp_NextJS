24e1b3d8d5730d52fd7c1cb33f7d7be598f54db2
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-26 15:26:49 -0300

    criando estrutura html estoque

--- src/pages/stock/index.tsx
+++ src/pages/stock/index.tsx
@@ -2,7 +2,35 @@ import type { NextPage } from 'next'
 import { Base } from '../../components/template'
 
 const Stock: NextPage = () => {
-  return <Base>Hello world</Base>
+  return (
+    <Base>
+      <div>
+        <div>
+          Estoques
+        </div>
+        <div>
+          menu de cada estoque
+
+          <div>
+            estoque details
+            <div>
+              produto
+              <span>Quantidade em estoque</span>
+            </div>
+            <div>
+              categoria
+            </div>
+            <div>
+              fornecedor
+              <span>dados do fornecedor</span>
+              <span>telefone</span>
+              <span>email</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Base>
+  )
 }
 
 export default Stock
