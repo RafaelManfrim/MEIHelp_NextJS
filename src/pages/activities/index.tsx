57e9398dfc9ce2eece6cf3b40b56c05feb4d26b2
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-26 14:36:58 -0300

    iniciando implementação do react-modal e react-hook-form

--- src/pages/activities/index.tsx
+++ src/pages/activities/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
-import { IoCheckmarkSharp, IoPencilSharp, IoTrashSharp } from 'react-icons/io5'
+import { IoCheckmarkSharp, IoPencilSharp, IoTrashSharp, IoCloseSharp } from 'react-icons/io5'
+import Modal from "react-modal"
+import { useForm } from 'react-hook-form'
+
 import { Button } from '../../components/Button'
 import { Base } from '../../components/template'
 import { api } from '../../services/api'
@@ -33,8 +36,28 @@ interface Activity {
 
 const Activities: NextPage = () => {
   const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivityId, setSelectedActivityId] = useState(0)
+  const [isCreatingActivityModalOpen, setIsCreatingActivityModalOpen] = useState(false)
+  const [isEditingActivityModalOpen, setIsEditingActivityModalOpen] = useState(false)
+  const [isDeletingActivityModalOpen, setIsDeletingActivityModalOpen] = useState(false)
 
-  async function handleCreateActivity() { }
+  const selectedActivity = activities.find(activity => activity.id === selectedActivityId)
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {}
+  })
+
+  function handleChangeCreateActivityModal() {
+    setIsCreatingActivityModalOpen(oldState => !oldState)
+  }
+
+  async function handleCreateActivity(data) {
+    console.log("Creating activity")
+  }
+
+  function handleChangeFinishActivityModal() {
+    setIsCreatingActivityModalOpen(oldState => !oldState)
+  }
 
   async function handleFinishActivity(id: number) {
     try {
@@ -53,7 +76,16 @@ const Activities: NextPage = () => {
     }
   }
 
+  function handleChangeEditActivityModal() {
+    setIsCreatingActivityModalOpen(oldState => !oldState)
+  }
+
   async function handleEditActivity(id: number) { }
+
+  function handleChangeDeleteActivityModal() {
+    setIsDeletingActivityModalOpen(oldState => !oldState)
+  }
+
 
   async function handleDeleteActivity(id: number) { }
 
@@ -67,56 +99,91 @@ const Activities: NextPage = () => {
   }, [])
 
   return (
-    <Base>
-      <ActivitiesContainer>
-        <Button text="Cadastrar nova atividade" />
-        {activities.map((activity) => (
-          <ActivityContainer key={activity.id}>
-            <ActivityStatusColor finished={activity.finished} />
-            <ActivityDetails>
-              <ActivityTitle>{activity.title}</ActivityTitle>
-              <ActivityDescription>
-                {activity.description
-                  ? activity.description
-                  : 'Não há descrição.'}
-              </ActivityDescription>
-            </ActivityDetails>
-            <ActivityInfos>
-              <ActivityButtons>
-                {!activity.finished && (
-                  <>
-                    <ActivityButton
-                      color="green-light"
-                      onClick={() => handleFinishActivity(activity.id)}
-                    >
-                      <IoCheckmarkSharp />
-                    </ActivityButton>
-                    <ActivityButton color="yellow">
-                      <IoPencilSharp />
-                    </ActivityButton>
-                  </>
-                )}
-                <ActivityButton color="red">
-                  <IoTrashSharp />
-                </ActivityButton>
-              </ActivityButtons>
-              <ActivityCreatedAt>
-                Criado em: {new Date(activity.created_at).toLocaleDateString()}
-              </ActivityCreatedAt>
-              <ActivityStatus>
-                {activity.finished
-                  ? `Concluído em: ${new Date(
-                    activity.finished_at,
-                  ).toLocaleDateString()}`
-                  : `Previsto para: ${new Date(
-                    activity.forecast_date,
-                  ).toLocaleDateString()}`}
-              </ActivityStatus>
-            </ActivityInfos>
-          </ActivityContainer>
-        ))}
-      </ActivitiesContainer>
-    </Base>
+    <>
+      <Base>
+        <ActivitiesContainer>
+          <Button text="Cadastrar nova atividade" onClick={handleCreateActivity} />
+          {activities.map((activity) => (
+            <ActivityContainer key={activity.id}>
+              <ActivityStatusColor finished={activity.finished} />
+              <ActivityDetails>
+                <ActivityTitle>{activity.title}</ActivityTitle>
+                <ActivityDescription>
+                  {activity.description
+                    ? activity.description
+                    : 'Não há descrição.'}
+                </ActivityDescription>
+              </ActivityDetails>
+              <ActivityInfos>
+                <ActivityButtons>
+                  {!activity.finished && (
+                    <>
+                      <ActivityButton
+                        color="green-light"
+                        onClick={() => handleFinishActivity(activity.id)}
+                      >
+                        <IoCheckmarkSharp />
+                      </ActivityButton>
+                      <ActivityButton color="yellow">
+                        <IoPencilSharp />
+                      </ActivityButton>
+                    </>
+                  )}
+                  <ActivityButton color="red" onClick={handleChangeDeleteActivityModal}>
+                    <IoTrashSharp />
+                  </ActivityButton>
+                </ActivityButtons>
+                <ActivityCreatedAt>
+                  Criado em: {new Date(activity.created_at).toLocaleDateString()}
+                </ActivityCreatedAt>
+                <ActivityStatus>
+                  {activity.finished
+                    ? `Concluído em: ${new Date(
+                      activity.finished_at,
+                    ).toLocaleDateString()}`
+                    : `Previsto para: ${new Date(
+                      activity.forecast_date,
+                    ).toLocaleDateString()}`}
+                </ActivityStatus>
+              </ActivityInfos>
+            </ActivityContainer>
+          ))}
+        </ActivitiesContainer>
+      </Base>
+      <Modal
+        isOpen={isCreatingActivityModalOpen}
+        onRequestClose={handleChangeCreateActivityModal}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+      >
+        <button type="button" onClick={handleChangeCreateActivityModal} className="react-modal-close">
+          <IoCloseSharp size={20} />
+        </button>
+        <form onSubmit={handleSubmit(handleCreateActivity)}>
+          <input type="text" placeholder="titulo" />
+          <input type="text" placeholder="descrição" />
+          <input type="text" placeholder="planejado para:" />
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Modal>
+      {/* <Modal isOpen={isEditingActivityModalOpen}>
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <IoCloseSharp size={20} />
+            </button>
+        editando
+      </Modal> */}
+      <Modal isOpen={isDeletingActivityModalOpen} onRequestClose={handleChangeDeleteActivityModal} overlayClassName="react-modal-overlay"
+        className="react-modal-content">
+        <button type="button" onClick={handleChangeDeleteActivityModal} className="react-modal-close">
+          <IoCloseSharp size={20} />
+        </button>
+        Tem certeza que deseja excluir a atividade?
+        <div>
+          <button type="button">Cancelar</button>
+          <button type="submit">Excluir</button>
+        </div>
+      </Modal>
+    </>
   )
 }
 
