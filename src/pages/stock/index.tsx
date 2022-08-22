84e1ed26771c4cce1cec2f7bc38580ddac386f0d
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-08-22 16:17:01 -0300

    Iniciando visual para estoques

--- src/pages/stock/index.tsx
+++ src/pages/stock/index.tsx
@@ -1,34 +1,75 @@
 import type { NextPage } from 'next'
+import { Button } from '../../components/Button'
+
 import { Base } from '../../components/template'
 
+import { CreateStockButton, MainStocksContainer, StockContainer, StocksContainer, StocksContainerHeader, StocksContainerTitle } from './styles'
+
 const Stock: NextPage = () => {
+  const teste = [1, 2, 3]
+
   return (
     <Base>
-      <div>
-        <div>
-          Estoques
-        </div>
-        <div>
-          menu de cada estoque
+      <MainStocksContainer>
+        <StocksContainerHeader>
+          <StocksContainerTitle>
+            Estoques
+          </StocksContainerTitle>
+          <CreateStockButton>
+            Criar um estoque
+          </CreateStockButton>
+        </StocksContainerHeader>
+        <StocksContainer>
+          {teste.map(test => (
+            <StockContainer key={test}>
+              Nome do estoque
 
-          <div>
-            estoque details
-            <div>
-              produto
-              <span>Quantidade em estoque</span>
-            </div>
-            <div>
-              categoria
-            </div>
-            <div>
-              fornecedor
-              <span>dados do fornecedor</span>
-              <span>telefone</span>
-              <span>email</span>
-            </div>
-          </div>
-        </div>
-      </div>
+              <div>
+                Detalhes do estoque
+                <div>
+                  produto
+                  <span>qtde</span>
+                </div>
+                <div>
+                  categoria
+                </div>
+                <div>
+                  fornecedor
+                  <span>dados do fornecedor</span>
+                  <span>telefone</span>
+                  <span>email</span>
+                </div>
+                <div>
+                  produto
+                  <span>qtde</span>
+                </div>
+                <div>
+                  categoria
+                </div>
+                <div>
+                  fornecedor
+                  <span>dados do fornecedor</span>
+                  <span>telefone</span>
+                  <span>email</span>
+                </div>
+                <div>
+                  produto
+                  <span>qtde</span>
+                </div>
+                <div>
+                  categoria
+                </div>
+                <div>
+                  fornecedor
+                  <span>dados do fornecedor</span>
+                  <span>telefone</span>
+                  <span>email</span>
+                </div>
+              </div>
+            </StockContainer>
+          ))}
+        </StocksContainer>
+      </MainStocksContainer>
     </Base>
   )
 }
--- src/pages/stock/styles.ts
+++ src/pages/stock/styles.ts
@@ -1,1 +1,50 @@
 import styled from 'styled-components'
+
+export const MainStocksContainer = styled.div`
+  width: 100%;
+`
+
+export const StocksContainerHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
+export const StocksContainerTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`
+export const CreateStockButton = styled.div`
+  padding: 0 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 28px;
+  background-color: var(--light-blue);
+  outline: var(--blue);
+  border: var(--blue);
+  border-radius: 0.25rem;
+  color: var(--white);
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`
+
+export const StocksContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.5rem;
+`
+
+export const StockContainer = styled.div`
+  background-color: var(--white);
+
+  width: 100%;
+  padding: 1rem;
+  border-radius: 8px;
+`
